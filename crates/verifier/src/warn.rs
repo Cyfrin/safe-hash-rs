9bb119fc0697a27f53c9c26ba79efe80ad8994da
TilakMaddy <tilak.madichetti@gmail.com>
2025-03-02 14:15:46 +0530

    feat: add warnings for function selector

--- crates/verifier/src/warn.rs
+++ crates/verifier/src/warn.rs
@@ -1,4 +1,7 @@
-use alloy::primitives::{Address, U256};
+use alloy::{
+    hex,
+    primitives::{Address, U256, keccak256},
+};
 
 use crate::tx_file::TenderlyTxInput;
 
@@ -32,8 +35,51 @@ pub fn warn_suspicious_content(tx_data: &TenderlyTxInput) {
         warnings.push("Gas price is non zero for the transaction, it increases potential for hidden value transfer.");
     }
 
+    // Check calldata
+    if is_suspicous_calldata(tx_data.data.clone()) {
+        warnings.push("Suspicious calldata found. It may potentially modify the owners or the threshold of the safe.");
+    }
+
+    // Print warnings
     if !warnings.is_empty() {
-        println!("WARNINGS:");
-        warnings.iter().for_each(|line| println!("{}", line));
+        println!("\nWARNINGS:\n");
+        warnings.iter().for_each(|line| println!("{}\n", line));
+    }
+}
+
+const SUSPICIOUS_FUNC_SIGNATURES: &[&str] = &[
+    "addOwnerWithThreshold(address,uint256)",
+    "removeOwner(address,address,uint256)",
+    "swapOwner(address,address,address)",
+    "changeThreshold(uint256)",
+];
+
+fn is_suspicous_calldata(calldata: String) -> bool {
+    let suspicous_func_selectors = SUSPICIOUS_FUNC_SIGNATURES
+        .iter()
+        .map(|s| {
+            let func_hash = keccak256(s);
+            func_hash[..4].to_vec()
+        })
+        .collect::<Vec<_>>();
+
+    let decoded_calldata = hex::decode(calldata).expect("unable to decode calldata");
+    let first4bytes = &decoded_calldata[..4];
+
+    suspicous_func_selectors.iter().any(|s| s == first4bytes)
+}
+
+#[cfg(test)]
+mod suspicious_func_selector_tests {
+
+    use super::*;
+
+    #[test]
+    fn test_func_selector() {
+        // cast calldata "changeThreshold(uint256)" 12314
+        let threshold_changing_calldata =
+            "0x694e80c3000000000000000000000000000000000000000000000000000000000000301a";
+
+        assert!(is_suspicous_calldata(threshold_changing_calldata.to_string()));
     }
 }
