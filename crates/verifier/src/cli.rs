24a6ccb4fd28c3e914e007c2a7d46ab113065103
TilakMaddy <tilak.madichetti@gmail.com>
2025-03-03 15:30:12 +0530

    WIP

--- crates/verifier/src/cli.rs
+++ crates/verifier/src/cli.rs
@@ -1,8 +1,7 @@
-use std::path::PathBuf;
-
-use alloy::primitives::Address;
+use alloy::primitives::{Address, U256};
 use clap::Parser;
 use safe_utils::{SafeWalletVersion, get_all_supported_chain_names};
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -22,28 +21,72 @@ pub struct CliArgs {
     #[arg(short, long)]
     pub safe_contract: Option<Address>,
 
-    /// Path to JSON file containing the input from Tenderly's simulation summary
-    #[arg(short, long)]
+    /// Safe Contract version
+    #[arg(short = 'u', long, default_value = "1.3.0")]
+    pub safe_version: SafeWalletVersion,
+
+    /// Path to JSON file containing all the transaction data [clean way] 
+    /// Most suitable if copying the input from Tenderly.
+    ///
+    /// The alternative, is to pass the tx details yourself as individual command line arguments.
+    /// This includes `to` and optionally `value`, `data`, `operation`, `safe-tx-gas`, `base-gas`, `gas-price`,
+    /// `gas-token`, `refund-receiver`, `signatures`
+    #[arg(
+        short,
+        long, 
+        conflicts_with_all=["to", "value", "data", "operation", "safe_tx_gas", "base_gas", "gas_price", "gas_token", "refund_receiver"]
+    )]
     pub tx_file: Option<PathBuf>,
 
-    /// Path to message file for offchain message hashes
+    /// Address of the contract to which the safe-contract sends calldata to.
+    #[arg(long)]
+    pub to: Option<Address>,
+
+    /// Value asked in the transaction (relates to eth)
+    #[arg(long, default_value_t = U256::ZERO, requires="to")]
+    pub value: U256,
+
+    /// Raw calldata encoded in hex
+    #[arg(long, default_value = "0x", requires="to")]
+    pub data: String,
+
+    /// Call or delegate call (0 or 1)
+    #[arg(long, default_value_t = 0, requires="to")]
+    pub operation: u8,
+
+    #[arg(long, default_value_t = U256::ZERO, requires="to")]
+    pub safe_tx_gas: U256,
+
+    #[arg(long, default_value_t = U256::ZERO, requires="to")]
+    pub base_gas: U256,
+
+    #[arg(long, default_value_t = U256::ZERO, requires="to")]
+    pub gas_price: U256,
+
+    #[arg(long, default_value_t = Address::ZERO, requires="to")]
+    pub gas_token: Address,
+
+    #[arg(long, default_value_t = Address::ZERO, requires="to")]
+    pub refund_receiver: Address,
+
+    /// Required when checking for execution
+    #[arg(long)]
+    pub signatures: Option<String>,
+
+    /// Path to message file containing message in plain text
     #[arg(short, long)]
     pub message_file: Option<PathBuf>,
 
-    /// Safe Contract version
-    #[arg(long, default_value = "1.3.0")]
-    pub safe_version: SafeWalletVersion,
-
     /// Check for signing the transaction
-    #[arg(long, group = "check_for")]
+    #[arg(short = 'k', long, group = "check_for")]
     pub check_for_signing: bool,
 
     /// Check for executing the transaction
-    #[arg(long, group = "check_for")]
+    #[arg(short = 'x', long, group = "check_for")]
     pub check_for_executing: bool,
 
     /// Check message hashes off-chain
-    #[arg(long, group = "check_for")]
+    #[arg(short = 'o', long, group = "check_for")]
     pub check_for_message_hash: bool,
 }
 
